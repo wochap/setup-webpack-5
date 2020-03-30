492f9944f00056424ee0d2ed9d929760b8934656
Gean <gean.marroquin@gmail.com>
2020-03-30 15:36:27 -0500

    feat: add babel-loader
    https://www.zzuu666.com/articles/9

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,18 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true
+        }
+      }
+    }]
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
