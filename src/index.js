63288ffeeb062b725efb0b2852f9f78cd3eacd8d
Gean <gean.marroquin@gmail.com>
2020-03-30 15:54:40 -0500

    feat: add css support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './index.css'
 import BAR from "./module-b"
 
 const a = 123
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const webpack = require('webpack')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -19,12 +19,16 @@ module.exports = {
           cacheDirectory: true
         }
       }
+    }, {
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
     }]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "src/index.html",
-    })
+    }),
+    new MiniCssExtractPlugin(),
   ],
 };
