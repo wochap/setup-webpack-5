c6910ba13c12fefa7a8474c1a0a48368ecb594ff
Gean <gean.marroquin@gmail.com>
2020-03-30 14:00:53 -0500

    feat: add html-webpack-plugin and clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require('webpack')
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +9,10 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    })
+  ],
 };
